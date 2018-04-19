a566bfc251639f47fe7ec83a79f90d5463221e25
Bobae Kang <Bobae.Kang@illinois.gov>
2018-04-19 13:09:07 -0500

    edit dockerfile and make minor changes

--- app/ui.R
+++ app/ui.R
@@ -1,6 +1,6 @@
 # Author: Bobae Kang (Bobae.Kang@illinois.gov)
 # Created: 2017-10-31
-# Last revised: 2018-04-17
+# Last revised: 2018-04-19
 # Script title: ui.R
 #---------------------------------------------------------------------------
 # Script description:
