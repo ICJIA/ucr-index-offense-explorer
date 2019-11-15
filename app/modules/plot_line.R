43a5480d6745478c4300b76ccfd813dccb92f59c
Bobae Kang <bkang.dev@gmail.com>
2019-11-15 15:26:49 -0600

    feat: show reporting county # in tooltip

--- app/modules/plot_line.R
+++ app/modules/plot_line.R
@@ -5,18 +5,28 @@ plot_line <- function(input, output, data_reactive) {
   })
 
   output$line <- renderHighchart({
+    reporting <-
+      data_reactive() %>%
+      na.omit() %>%
+      group_by(year) %>%
+      count(name = 'reporting')
+    
     data <-
       data_reactive() %>%
       group_by(year) %>%
       summarise_at(
         vars(Person = person_crime, Property = property_crime),
         function(x) {
-          if (input$unit == "Count") sum(x, na.rm = TRUE)
-          else apply_rate(sum(x, na.rm = TRUE), sum(.$population))
+          sum2 <-
+            if (input$county == "All") function(x) sum(x, na.rm = TRUE)
+            else sum
+          if (input$unit == "Count") sum2(x)
+          else apply_rate(sum2(x), sum(.$population))
         }
-      )
+      ) %>%
+      left_join(reporting, by = "year")
 
-    highchart() %>% 
+    highchart() %>%
       {
         if (input$category == "All") {
           hc_add_series(
@@ -24,7 +34,7 @@ plot_line <- function(input, output, data_reactive) {
             name  = "Person",
             data  = data$Person,
             color = "#f45b5b"
-          ) %>% 
+          ) %>%
           hc_add_series(
             name  = "Property",
             data  = data$Property,
@@ -40,12 +50,33 @@ plot_line <- function(input, output, data_reactive) {
           hc_legend(enabled = FALSE)
         }
       } %>%
+      hc_add_series(
+        name = "Reporting county",
+        data = data$reporting,
+        color = "#fff",
+        dashStyle = "Dot",
+        lineWidth = 0,
+        marker = list(
+          radius = 0
+        ),
+        showInLegend = FALSE,
+        states = list(
+          hover = list(
+            enabled = FALSE
+          )
+        )
+      ) %>%
+      hc_tooltip(shared = TRUE) %>%
       hc_xAxis(categories = data$year) %>%
       hc_yAxis(
         title = list(
           text = if (input$unit == "Count") "count" else "rate (per 100k)"
         ),
-        min = 0
+        min = 0,
+        max = {
+          if (input$category == "All") max(data$Property)
+          else max(data[[input$category]])
+        }
       ) %>%
       hc_add_theme(hc_theme_sandsignika())
   })
